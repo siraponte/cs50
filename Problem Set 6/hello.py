c4fa6fe7f2c57a75335d3e94bd88e85872d02324
Ciro Seraponte <73239165+siraponte@users.noreply.github.com>
2020-12-28 12:23:23 +0100

    Create hello.py

---
+++ Problem Set 6/hello.py
@@ -0,0 +1,10 @@
+# Made by siraponte
+
+# import get_string from cs50 library
+from cs50 import get_string
+
+# ask user's name
+answer = get_string("What's your name?\n")
+
+# print "hello, user's name"
+print(f"hello, {answer}")
